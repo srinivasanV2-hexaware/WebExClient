f652add8ae11335a20c010ab742342eb1ec8aa70
srinivasan velu <31004655+srinivasanV2-hexaware@users.noreply.github.com>
2018-03-05 21:03:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-var request = require('request');
 var express = require('express');
 var app = express();
-const xmlQuery = require('xml-query');
-const XmlReader = require('xml-reader');
-var Promise = require('promise');
 var createMeeting=require('./createmeeting');
 
 app.get("/", function (req, res) {
