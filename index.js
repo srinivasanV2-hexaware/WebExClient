264c8f655ed7f080c2b6c34299d984f1ce7e8f14
srinivasan velu <31004655+srinivasanV2-hexaware@users.noreply.github.com>
2018-03-05 22:09:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 var CreateMeeting = require('./createmeeting');
 
 app.get("/", function (req, res) {
-    CreateMeeting.SendMeeting('Nuance Meeting','LKV Cabin', '', ['ursfriendly_sri@yahoo.com','38238.hexaware@gmail.com'], '2018-03-06,05:00:00', '2018-03-06,05:30:00',session).then(function (result) {
+    CreateMeeting.SendMeeting('Nuance Meeting','LKV Cabin', '', ['ursfriendly_sri@yahoo.com','38238.hexaware@gmail.com'], '2018-03-06,05:00:00', '2018-03-06,05:30:00','session').then(function (result) {
         res.end("success");
     }).catch(function (errdata) {
         res.end(errdata);
