be92e69edbe6556d66700acd06734d2d02a1fdf4
srinivasan velu <31004655+srinivasanV2-hexaware@users.noreply.github.com>
2018-03-05 21:12:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 var createMeeting=require('./createmeeting');
 
 app.get("/", function (req, res) {
-    SendMeeting().then(function (result) {
+    createMeeting.SendMeeting().then(function (result) {
         res.end("success");
     }).catch(function (errdata) {
         res.end(errdata);
